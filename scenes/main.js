c3ef71f15aa663dc5bb39f0a1887b9f9f7ecbd7c
Ganga Singh <43028931+meganga@users.noreply.github.com>
2021-06-16 14:52:13 +0000

    Added the collides method which makes the 'space-invaders' to stay within the walls.

--- scenes/main.js
+++ scenes/main.js
@@ -17,9 +17,9 @@ addLevel([
 ], {
     width: 30,
     height: 22,
-    '^': [sprite('space-invader'), scale(0.7)],
-    '!': [sprite('wall'), 'left'],
-    '&': [sprite('wall'), 'right'],
+    '^': [sprite('space-invader'), scale(0.7), 'space-invader'],
+    '!': [sprite('wall'), 'left-wall'],
+    '&': [sprite('wall'), 'right-wall'],
   })
 
 const player = add([
@@ -49,23 +49,50 @@ const score = add([
 ])
 
 
-const TIME_LEFT = 14
+// const TIME_LEFT = 100
 
 
-const timmer = add([
-  text('0'),
-  pos(90, 50),
-  scale(2),
-  layer('ui'),
-  {
-    time: TIME_LEFT,
-  },
-])
+// const timer = add([
+//   text('0'),
+//   pos(90, 50),
+//   scale(2),
+//   layer('ui'),
+//   {
+//     time: TIME_LEFT,
+//   },
+// ])
 
 
-timer.action(() => {
-  timer.time -= dt()
-  timer.text = timer.time
+// timer.action(() => {
+//   timer.time -= dt()
+//   timer.text = timer.time.toFixed(2)
+//   if(timer.time <= 0){
+//     go('lose', score.value)
+//   }
+// })
+
+const INVADER_SPEED = 100
+let CURRENT_SPEED = INVADER_SPEED
+const LEVEL_DOWN = 350
+
+action('space-invader', (s)=>{
+  s.move(CURRENT_SPEED, 0)
 })
 
 
+collides('space-invader', 'right-wall', ()=>{
+  CURRENT_SPEED = -INVADER_SPEED
+  every('space-invader', (s)=>{
+    s.move(0, LEVEL_DOWN)
+  })
+
+})
+
+
+collides('space-invader', 'left-wall', ()=>{
+  CURRENT_SPEED = INVADER_SPEED
+  every('space-invader', (s)=>{
+    s.move(0, LEVEL_DOWN)
+  })
+
+})
